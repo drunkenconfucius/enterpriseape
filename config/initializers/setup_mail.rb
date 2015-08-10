2a9fcff3d7a984f25d1d2695735683c559a78af1
drunkenconfucius <scott@scottely.com>
2015-08-10 00:48:44 +0000

    updated secure sendgrid

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,8 +3,8 @@ ActionMailer::Base.smtp_settings = {
   :address          =>  'smtp.sendgrid.net',
   :port             =>  '587',
   :authentication   =>  :plain,
-  :user_name        =>  'app39855859@heroku.com',
-  :user_password    =>  'ue8ornki9629',
+  :user_name        =>  ENV['SENDGRID_USERNAME'],
+  :user_password    =>  ENV['SENDGRID_PASSWORD'],
   :domain           =>  'heroku.com',
   :enable_starttls_auto  =>  true
 }
