a606a502ad63ba4b0aee941b934da4b243cb060d
drunkenconfucius <scott@scottely.com>
2015-08-11 14:27:55 +0000

    trying to fix heroku user page issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,8 +77,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => 'enterpriseapedrunkenconfucius.herokuapp.com:3000'}
+ # config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'enterpriseapedrunkenconfucius.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
 end
