6fb5d8621e7ff34ce7ac897d13220d3e2a85077d
drunkenconfucius <scott@scottely.com>
2015-08-13 19:16:52 +0000

    updated production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,8 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
  # config.action_mailer.raise_delivery_errors = true
-  Rails.application.routes.default_url_options [:host] = 'https://enterpriseapedrunkenconfucius.herokuapp.com/'
+  Rails.application.routes.default_url_options[:host] = 'enterpriseapedrunkenconfucius.herokuapp.com'
+  #Rails.application.routes.default_url_options [:host] = 'https://enterpriseapedrunkenconfucius.herokuapp.com/'
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
